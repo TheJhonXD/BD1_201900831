de84495df5d4013ca4b4b206df22a0c9b4a43cc5
Jhon <3227943820801@ingenieria.usac.edu.gt>
2023-09-16 14:10:28 -0600

    script sql

---
+++ API/src/db/script_bd.sql
@@ -0,0 +1,327 @@
+-- CREAR BASE DE DATOS
+
+CREATE SCHEMA IF NOT EXISTS bd1_p1;
+
+--  TABLA PARTIDO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.PARTIDO (
+  id INTEGER NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  siglas VARCHAR(10) NOT NULL,
+  fundacion DATE NOT NULL,
+  PRIMARY KEY (id) 
+);
+
+-- TABLA CARGO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.CARGO (
+  id INTEGER NOT NULL,
+  cargo VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- TABLA CANDIDATO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.CANDIDATO (
+  id INTEGER NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  fecha_nac DATE NOT NULL,
+  id_partido INTEGER NOT NULL,
+  id_cargo INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_partido) REFERENCES bd1_p1.PARTIDO (id),
+  FOREIGN KEY (id_cargo) REFERENCES bd1_p1.CARGO (id)
+);
+
+
+-- TABLA CIUDADANO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.CIUDADANO (
+  dpi VARCHAR(13) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  direccion VARCHAR(100) NOT NULL,
+  telefono VARCHAR(10) NOT NULL,
+  edad INTEGER NOT NULL,
+  genero VARCHAR(1) NOT NULL,
+  PRIMARY KEY (dpi)
+);
+
+-- TABLA DEPARTAMENTO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.DEPARTAMENTO (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- TABLA MESA
+
+CREATE TABLE IF NOT EXISTS bd1_p1.MESA (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  id_dep INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_dep) REFERENCES bd1_p1.DEPARTAMENTO (id)
+);
+
+-- TABLA VOTO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.VOTO (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  dpi VARCHAR(13) NOT NULL,
+  id_mesa INTEGER NOT NULL,
+  fechahora DATETIME NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (dpi) REFERENCES bd1_p1.CIUDADANO (dpi),
+  FOREIGN KEY (id_mesa) REFERENCES bd1_p1.MESA (id)
+);
+
+-- TABLA DETALLE VOTO
+
+CREATE TABLE IF NOT EXISTS bd1_p1.DETALLE_VOTO (
+  id_voto INTEGER NOT NULL,
+  id_candidato INTEGER NOT NULL,
+  FOREIGN KEY (id_voto) REFERENCES bd1_p1.VOTO (id),
+  FOREIGN KEY (id_candidato) REFERENCES bd1_p1.CANDIDATO (id)
+);
+
+-- ELIMINAR MODELO
+-- BORRAR BASE DE DATOS
+DROP DATABASE IF EXISTS bd1_p1;
+-- BORRAR TABLA PARTIDO
+DROP TABLE IF EXISTS bd1_p1.PARTIDO;
+-- BORRAR TABLA CARGO
+DROP TABLE IF EXISTS bd1_p1.CARGO;
+-- BORRAR TABLA CANDIDATO
+DROP TABLE IF EXISTS bd1_p1.CANDIDATO;
+-- BORRAR TABLA CIUDADANO
+DROP TABLE IF EXISTS bd1_p1.CIUDADANO;
+-- BORRAR TABLA DEPARTAMENTO
+DROP TABLE IF EXISTS bd1_p1.DEPARTAMENTO;
+-- BORRAR TABLA MESA
+DROP TABLE IF EXISTS bd1_p1.MESA;
+-- BORRAR TABLA VOTO
+DROP TABLE IF EXISTS bd1_p1.VOTO;
+-- BORRAR TABLA DETALLE VOTO
+DROP TABLE IF EXISTS bd1_p1.DETALLE_VOTO;
+
+-- +++ TABLAS TEMPORALES +++
+-- Tabla temporal para PARTIDO
+  CREATE TEMPORARY TABLE bd1_p1.PARTIDO_TMP (
+    id INTEGER NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    siglas VARCHAR(10) NOT NULL,
+    fundacion DATE NOT NULL
+  );
+
+  -- Tabla temporal para CARGO
+  CREATE TEMPORARY TABLE bd1_p1.CARGO_TMP (
+    id INTEGER NOT NULL,
+    cargo VARCHAR(50) NOT NULL
+  );
+
+  -- Tabla temporal para CANDIDATO
+  CREATE TEMPORARY TABLE bd1_p1.CANDIDATO_TMP (
+    id INTEGER NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_nac DATE NOT NULL,
+    id_partido INTEGER NOT NULL,
+    id_cargo INTEGER NOT NULL
+  );
+
+  -- Tabla temporal para CIUDADANO
+  CREATE TEMPORARY TABLE bd1_p1.CIUDADANO_TMP (
+    dpi VARCHAR(13) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    telefono VARCHAR(10) NOT NULL,
+    edad INTEGER NOT NULL,
+    genero VARCHAR(1) NOT NULL
+  );
+
+  -- Tabla temporal para DEPARTAMENTO
+  CREATE TEMPORARY TABLE bd1_p1.DEPARTAMENTO_TMP (
+    nombre VARCHAR(20) NOT NULL
+  );
+
+  -- Tabla temporal para MESA
+  CREATE TEMPORARY TABLE bd1_p1.MESA_TMP (
+    id_dep INTEGER NOT NULL
+  );
+
+  -- Tabla temporal para VOTO
+  CREATE TEMPORARY TABLE bd1_p1.VOTO_TMP (
+    dpi VARCHAR(13) NOT NULL,
+    id_mesa INTEGER NOT NULL,
+    fechahora DATETIME NOT NULL
+  );
+
+  -- Tabla temporal para DETALLE VOTO
+  CREATE TEMPORARY TABLE bd1_p1.DETALLE_VOTO_TMP (
+    id_voto INTEGER NOT NULL,
+    id_candidato INTEGER NOT NULL
+  );
+
+-- +++ CONSULTAS CREADAS +++
+-- CONSULTA 1
+SELECT 
+    Presi.nombre AS "Presidente",
+    Vice.nombre AS "Vicepresidente",
+    PARTIDO.nombre AS "Partido"
+  FROM 
+    bd1_p1.CANDIDATO AS Presi
+  JOIN 
+    bd1_p1.CANDIDATO AS Vice ON Presi.id_partido = Vice.id_partido AND Presi.id <> Vice.id
+  JOIN 
+    bd1_p1.PARTIDO ON Presi.id_partido = PARTIDO.id
+  WHERE 
+    Presi.id_cargo = 1 AND Vice.id_cargo = 2;
+
+-- CONSULTA 2
+ SELECT 
+    PARTIDO.nombre AS "Partido",
+    COUNT(*) AS "Cantidad"
+  FROM 
+    bd1_p1.CANDIDATO
+  JOIN 
+    bd1_p1.PARTIDO ON CANDIDATO.id_partido = PARTIDO.id
+  JOIN 
+    bd1_p1.CARGO ON CANDIDATO.id_cargo = CARGO.id
+  WHERE 
+    CARGO.cargo LIKE '%diputado%'
+  GROUP BY 
+    PARTIDO.nombre;
+
+-- CONSULTA 3
+SELECT 
+    CANDIDATO.nombre AS "Nombre",
+    PARTIDO.nombre AS "Partido"
+  FROM 
+    bd1_p1.CANDIDATO
+  JOIN 
+    bd1_p1.PARTIDO ON CANDIDATO.id_partido = PARTIDO.id
+  JOIN 
+    bd1_p1.CARGO ON CANDIDATO.id_cargo = CARGO.id
+  WHERE 
+    CARGO.cargo = 'alcalde';
+
+-- CONSULTA 4
+SELECT 
+    PARTIDO.nombre AS "Partido",
+    COUNT(*) AS "Cantidad"
+  FROM 
+    bd1_p1.CANDIDATO
+  JOIN 
+    bd1_p1.PARTIDO ON CANDIDATO.id_partido = PARTIDO.id
+  JOIN 
+    bd1_p1.CARGO ON CANDIDATO.id_cargo = CARGO.id
+  GROUP BY 
+    PARTIDO.nombre;
+
+-- CONSULTA 5
+SELECT 
+    DEPARTAMENTO.nombre AS "Departamento",
+    COUNT(VOTO.id) AS "Cantidad_Votaciones"
+  FROM 
+    bd1_p1.VOTO
+  JOIN 
+    bd1_p1.MESA ON VOTO.id_mesa = MESA.id
+  JOIN 
+    bd1_p1.DEPARTAMENTO ON MESA.id_dep = DEPARTAMENTO.id
+  GROUP BY 
+    DEPARTAMENTO.nombre;
+
+-- CONSULTA 6
+SELECT 
+    COUNT(*) AS "Nulos"
+  FROM 
+    bd1_p1.DETALLE_VOTO
+  WHERE 
+    DETALLE_VOTO.id_candidato = -1;
+
+-- CONSULTA 7
+SELECT 
+	CIUDADANO.edad AS "Edad",
+    COUNT(*) AS "Cantidad"
+  FROM 
+    bd1_p1.CIUDADANO
+  JOIN
+	  bd1_p1.VOTO ON CIUDADANO.dpi = VOTO.dpi
+  GROUP BY
+	  CIUDADANO.edad
+  ORDER BY
+	  COUNT(*) DESC, CIUDADANO.edad 
+  LIMIT 10;
+
+-- CONSULTA 8
+SELECT 
+    Presi.nombre AS "Presidente",
+    Vice.nombre AS "Vicepresidente",
+    COUNT(*) AS "Votos_totales"
+  FROM 
+    bd1_p1.DETALLE_VOTO
+  JOIN 
+    bd1_p1.CANDIDATO AS Presi ON DETALLE_VOTO.id_candidato = Presi.id
+  JOIN 
+    bd1_p1.CANDIDATO AS Vice ON Vice.id_partido = Presi.id_partido
+  JOIN 
+    bd1_p1.VOTO ON DETALLE_VOTO.id_voto = VOTO.id
+  WHERE 
+    Presi.id_cargo = 1 AND Vice.id_cargo = 2
+  GROUP BY 
+    Presi.nombre, Vice.nombre
+  ORDER BY 
+    COUNT(*) DESC
+  LIMIT 10;
+
+-- CONSULTA 9
+SELECT 
+    MESA.id AS "No_Mesa",
+    DEPARTAMENTO.nombre AS "Departamento",
+    COUNT(*) AS "Cantidad_votos",
+    Frecuencia.freq AS "Frecuencia"
+  FROM 
+    bd1_p1.MESA
+  JOIN 
+    bd1_p1.DEPARTAMENTO ON MESA.id_dep = DEPARTAMENTO.id
+  JOIN 
+    bd1_p1.VOTO ON MESA.id = VOTO.id_mesa
+  JOIN 
+	  bd1_p1.DETALLE_VOTO ON VOTO.id = DETALLE_VOTO.id_voto
+  JOIN (
+    SELECT 
+      VOTO.id_mesa AS idmesa,
+      COUNT(*) AS freq
+    FROM 
+      bd1_p1.VOTO
+    GROUP BY 
+      bd1_p1.VOTO.id_mesa) AS Frecuencia ON MESA.id = Frecuencia.idmesa
+  GROUP BY 
+    MESA.id, DEPARTAMENTO.nombre, Frecuencia.freq
+  ORDER BY 
+    COUNT(*) DESC
+  LIMIT 5;
+
+-- CONSULTA 10
+SELECT 
+    DATE_FORMAT(VOTO.fechahora, '%H:%i') AS "Hora",
+    COUNT(*) AS "Cantidad_votos"
+  FROM 
+    bd1_p1.VOTO
+  GROUP BY 
+    DATE_FORMAT(fechahora, '%H:%i')
+  ORDER BY 
+    COUNT(*) DESC
+  LIMIT 5;
+
+-- CONSULTA 11
+SELECT 
+    CIUDADANO.genero AS "Genero",
+    COUNT(*) AS "Cantidad_votos"
+  FROM 
+    bd1_p1.VOTO
+  JOIN
+	  bd1_p1.CIUDADANO ON VOTO.dpi = CIUDADANO.dpi
+  GROUP BY 
+    CIUDADANO.genero;
